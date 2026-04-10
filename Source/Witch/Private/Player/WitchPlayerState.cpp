4b324f19c1d6628255cae49a22c2bebfded79a51
KoorinoMakoto <2421566657@qq.com>
2026-04-10 22:26:42 +0800

    The Player State

---
+++ Source/Witch/Private/Player/WitchPlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/WitchPlayerState.h"
+
+AWitchPlayerState::AWitchPlayerState()
+{
+	NetUpdateFrequency = 100.f;//服务器更新数据到客户端的频率
+}
---
+++ Source/Witch/Public/Player/WitchPlayerState.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "WitchPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WITCH_API AWitchPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AWitchPlayerState();
+};
