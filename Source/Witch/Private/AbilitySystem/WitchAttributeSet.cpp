9b2e8b75d2465b354966c96ac4ff1aef3446a4b1
KoorinoMakoto <2421566657@qq.com>
2026-04-12 17:15:12 +0800

    创建属性：Health,MaxHealth,Mana,MaxMana

--- Source/Witch/Private/AbilitySystem/WitchAttributeSet.cpp
+++ Source/Witch/Private/AbilitySystem/WitchAttributeSet.cpp
@@ -2,4 +2,43 @@
 
 
 #include "AbilitySystem/WitchAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UWitchAttributeSet::UWitchAttributeSet()
+{
+	
+}
+
+void UWitchAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	//注册 health 进行复制；COND_None：无条件复制；REPNOTIFY_Always：只要在服务器上设置了值，就要进行复制，不论与原来的值相比是否有变化
+	DOREPLIFETIME_CONDITION_NOTIFY(UWitchAttributeSet,Health,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWitchAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWitchAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWitchAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+	
+}
+//属性回调函数
+void UWitchAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	//告诉GAS系统，我们正在同步一个值，它的值刚从服务器拉取并改变，GAS记录这个变化，并记住旧值，以备需要回滚
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UWitchAttributeSet,Health, OldHealth);
+}
+
+void UWitchAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UWitchAttributeSet,MaxHealth, OldMaxHealth);
+}
+
+void UWitchAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UWitchAttributeSet,Mana, OldMana);
+}
+
+void UWitchAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UWitchAttributeSet,MaxMana, OldMaxMana);
+}
--- Source/Witch/Public/AbilitySystem/WitchAttributeSet.h
+++ Source/Witch/Public/AbilitySystem/WitchAttributeSet.h
@@ -13,5 +13,34 @@ UCLASS()
 class WITCH_API UWitchAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	UWitchAttributeSet();
+	//注册网络同步逻辑
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 	
+	//ReplicatedUsing：告诉引擎，当服务器上的 Health 发生变化并同步到客户端时，客户端不要只悄悄更新数值，还要触发 OnRep_Health 这个函数。
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health,Category="Vital Attributes")
+	//在 GAS 中，所有属性都必须是 FGameplayAttributeData 结构体，这个结构体内部其实维护了两个值：BaseValue（基础值）和 CurrentValue（当前值）。
+	FGameplayAttributeData Health;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth,Category="Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana,Category="Vital Attributes")
+	FGameplayAttributeData Mana;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana,Category="Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
