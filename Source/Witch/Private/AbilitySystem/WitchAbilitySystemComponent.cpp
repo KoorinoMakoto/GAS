c42a202cc2e64d208445c0764fae771b3cc66ea8
KoorinoMakoto <2421566657@qq.com>
2026-05-19 15:45:57 +0800

    获取当前GE所有的Asset Tags

--- Source/Witch/Private/AbilitySystem/WitchAbilitySystemComponent.cpp
+++ Source/Witch/Private/AbilitySystem/WitchAbilitySystemComponent.cpp
@@ -12,5 +12,15 @@ void UWitchAbilitySystemComponent::AbilityActorInfoSet()
 void UWitchAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
                                                  const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1,8.f, FColor::Green,FString("EffectApplied"));
+	//GEngine->AddOnScreenDebugMessage(1,8.f, FColor::Green,FString("EffectApplied"));
+	
+	FGameplayTagContainer TagContainer;
+	//不使用GrantedTags，而是使用AssetTags，后者可以传递标签：这些标签是我们在效果应用时希望获取到的
+	EffectSpec.GetAllAssetTags(TagContainer);
+	
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		//TODO: Broadcast the tag to the Widget Controller
+		
+	}
 }
